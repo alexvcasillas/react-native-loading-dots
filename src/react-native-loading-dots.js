511ccecec086bb6bb5166b3a48215ea75effa65f
Alex Casillas <alexvcasillas@gmail.com>
2019-06-16 13:18:19 +0200

    Launch time :rocket:

---
+++ src/react-native-loading-dots.js
@@ -0,0 +1,107 @@
+import React, { useState, useEffect, useRef } from "react";
+import { StyleSheet, Animated, Easing } from "react-native";
+
+const defaultColors = [
+  "#4dabf7",
+  "#3bc9db",
+  "#38d9a9",
+  "#69db7c"
+];
+
+function LoadingDots({ dots = 4, colors = defaultColors }) {
+  const [animations, setAnimations] = useState([]);
+  const [reverse, setReverse] = useState(false);
+
+  const opacity = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    const dotAnimations = [];
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < dots; i++) {
+      dotAnimations.push(new Animated.Value(0));
+    }
+    setAnimations(dotAnimations);
+  }, []);
+
+  useEffect(() => {
+    if (animations.length === 0) return;
+    loadingAnimation(animations, reverse);
+    appearAnimation();
+  }, [animations]);
+
+  function appearAnimation() {
+    Animated.timing(opacity, {
+      toValue: 1,
+      easing: Easing.ease,
+      useNativeDriver: true
+    }).start();
+  }
+
+  function floatAnimation(node, reverseY, delay) {
+    const floatSequence = Animated.sequence([
+      Animated.timing(node, {
+        toValue: reverseY ? 20 : -20,
+        easing: Easing.bezier(0.41, -0.15, 0.56, 1.21),
+        delay,
+        useNativeDriver: true
+      }),
+      Animated.timing(node, {
+        toValue: reverseY ? -20 : 20,
+        easing: Easing.bezier(0.41, -0.15, 0.56, 1.21),
+        delay,
+        useNativeDriver: true
+      }),
+      Animated.timing(node, {
+        toValue: 0,
+        delay,
+        useNativeDriver: true
+      })
+    ]);
+    return floatSequence;
+  }
+
+  function loadingAnimation(nodes, reverseY) {
+    Animated.parallel(
+      nodes.map((node, index) => floatAnimation(node, reverseY, index * 100))
+    ).start(() => {
+      setReverse(!reverse);
+    });
+  }
+
+  useEffect(() => {
+    if (animations.length === 0) return;
+    loadingAnimation(animations, reverse);
+  }, [reverse, animations]);
+
+  return (
+    <Animated.View style={[styles.loading, { opacity }]}>
+      {animations.map((animation, index) => (
+        <Animated.View
+          // eslint-disable-next-line react/no-array-index-key
+          key={`loading-anim-${index}`}
+          style={[
+            styles.dot,
+            { backgroundColor: colors[index] || "#4dabf7" },
+            { transform: [{ translateY: animation }] }
+          ]}
+        />
+      ))}
+    </Animated.View>
+  );
+}
+
+const styles = StyleSheet.create({
+  loading: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  dot: {
+    width: 20,
+    height: 20,
+    borderRadius: 10
+  }
+});
+
+export default LoadingDots;
